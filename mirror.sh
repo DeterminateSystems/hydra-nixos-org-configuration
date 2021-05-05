37c9011a68a8b96ce07b8293fd4f25d1e724eff4
Graham Christensen <graham@grahamc.com>
2021-05-05 07:49:33 -0400

    Use pinned sort?

--- mirror.sh
+++ mirror.sh
@@ -6,7 +6,7 @@ rm -rf config
 mkdir next
 nix-shell ./importer/shell.nix --run "./importer/generator.sh https://hydra.nixos.org ./next ./next/import"
 
-cat next/import | sort > next/import.sorted
+nix-shell ./importer/shell.nix --run "cat next/import | sort > next/import.sorted"
 mv next/import.sorted next/import
 
 mv next config
