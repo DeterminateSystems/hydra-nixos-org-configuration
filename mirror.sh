587214516bb80793608923282fdd68189b250d77
Graham Christensen <graham@grahamc.com>
2021-05-04 22:53:50 -0400

    mirror script: move, git add, sort imports

--- mirror.sh
+++ mirror.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+rm -rf config
+mkdir next
 nix-shell ./importer/shell.nix --run "./importer/generator.sh https://hydra.nixos.org ./next ./next/import"
+
+cat next/import | sort > next/import.sorted
+mv next/import.sorted next/import
+
+mv next config
+git add config
+git commit -m "Automatic re-import" || true
+
