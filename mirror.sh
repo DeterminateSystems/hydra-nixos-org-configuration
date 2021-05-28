3639e8b7063bffe342d800111d12bb1279ddc865
Graham Christensen <graham@grahamc.com>
2021-05-28 09:42:15 -0400

    Use the latest upstream importer

--- mirror.sh
+++ mirror.sh
@@ -4,6 +4,10 @@ export LANG=en_US.UTF-8
 
 set -eux
 
+rm -rf importer
+git clone https://github.com/DeterminateSystems/terraform-provider-hydra.git
+ln -s ./terraform-provider-hydra/tools ./importer
+
 rm -rf config
 mkdir next
 nix-shell ./importer/shell.nix --run "./importer/generator.sh https://hydra.nixos.org ./next ./next/import"
